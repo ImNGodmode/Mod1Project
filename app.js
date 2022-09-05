ee02c0d82e18df025dbd932dd3b5e8be13407d6c
ImNGodmode <bjames.56@icloud.com>
2022-09-05 13:33:20 -0400

    added zombie atk function that runs moveZombie# when the zombie# divs are populated with an img

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 
 let sec = 0
 let time = document.getElementById("time")
-
+let pos1 = 0
+let pos2 = 0
+let pos3 = 0
+let pos4 = 0
+let pos5 = 0
+let pos6 = 0
+let pos7 = 0
+let pos8 = 0
 
 const timer = () => {
    sec =30
@@ -19,14 +26,14 @@ const timer = () => {
 const moveZombie1 = () => {
    let id = null
    const zombie = document.getElementById('zombie1')
-   let pos = 0
+    pos1 = 0
    clearInterval(id)
    const frame = () => {
-      if (pos === 350) {
+      if (pos1 === 350) {
          clearInterval(id)
       }else {
-         pos++
-         zombie.style.left = pos + "px"
+         pos1++
+         zombie.style.left = pos1 + "px"
       }
    }
    id = setInterval(frame, 5)
@@ -34,14 +41,14 @@ const moveZombie1 = () => {
 const moveZombie2 = () => {
    let id = null
    const zombie = document.getElementById('zombie2')
-   let pos = 0
+   pos2 = 0
    clearInterval(id)
    const frame = () => {
-      if (pos === 350) {
+      if (pos2 === 350) {
          clearInterval(id)
       }else {
-         pos++
-         zombie.style.right = pos + "px"
+         pos2++
+         zombie.style.right = pos2 + "px"
       }
    }
    id = setInterval(frame, 5)
@@ -49,14 +56,14 @@ const moveZombie2 = () => {
 const moveZombie3 = () => {
    let id = null
    const zombie = document.getElementById('zombie3')
-   let pos = 0
+   pos3 = 0
    clearInterval(id)
    const frame = () => {
-      if (pos === 200) {
+      if (pos3 === 200) {
          clearInterval(id)
       }else {
-         pos++
-         zombie.style.top = pos + "px"
+         pos3++
+         zombie.style.top = pos3 + "px"
       }
    }
    id = setInterval(frame, 5)
@@ -64,14 +71,14 @@ const moveZombie3 = () => {
 const moveZombie4 = () => {
    let id = null
    const zombie = document.getElementById('zombie4')
-   let pos = 0
+   pos4 = 0
    clearInterval(id)
    const frame = () => {
-      if (pos === 200) {
+      if (pos4 === 200) {
          clearInterval(id)
       }else {
-         pos++
-         zombie.style.bottom = pos + "px"
+         pos4++
+         zombie.style.bottom = pos4 + "px"
       }
    }
    id = setInterval(frame, 5)
@@ -79,15 +86,15 @@ const moveZombie4 = () => {
 const moveZombie5 = () => {
    let id = null
    const zombie = document.getElementById('zombie5')
-   let pos = 0
+   pos5 = 0
    clearInterval(id)
    const frame = () => {
-      if (pos === 350) {
+      if (pos5 === 350) {
          clearInterval(id)
       }else {
-         pos++
-         zombie.style.right = pos + "px"
-         zombie.style.top = pos + "px"
+         pos5++
+         zombie.style.right = pos5 + "px"
+         zombie.style.top = pos5 + "px"
       }
    }
    id = setInterval(frame, 5)
@@ -95,15 +102,15 @@ const moveZombie5 = () => {
 const moveZombie6 = () => {
    let id = null
    const zombie = document.getElementById('zombie6')
-   let pos = 0
+   pos6 = 0
    clearInterval(id)
    const frame = () => {
-      if (pos === 350) {
+      if (pos6 === 350) {
          clearInterval(id)
       }else {
-         pos++
-         zombie.style.right = pos + "px"
-         zombie.style.bottom = pos + "px"
+         pos6++
+         zombie.style.right = pos6 + "px"
+         zombie.style.bottom = pos6 + "px"
       }
    }
    id = setInterval(frame, 5)
@@ -111,15 +118,15 @@ const moveZombie6 = () => {
 const moveZombie7 = () => {
    let id = null
    const zombie = document.getElementById('zombie7')
-   let pos = 0
+   pos7 = 0
    clearInterval(id)
    const frame = () => {
-      if (pos === 350) {
+      if (pos7 === 350) {
          clearInterval(id)
       }else {
-         pos++
-         zombie.style.left = pos + "px"
-         zombie.style.top = pos + "px"
+         pos7++
+         zombie.style.left = pos7 + "px"
+         zombie.style.top = pos7 + "px"
       }
    }
    id = setInterval(frame, 5)
@@ -127,25 +134,53 @@ const moveZombie7 = () => {
 const moveZombie8 = () => {
    let id = null
    const zombie = document.getElementById('zombie8')
-   let pos = 0
+   pos8 = 0
    clearInterval(id)
    const frame = () => {
-      if (pos === 350) {
+      if (pos8 === 350) {
          clearInterval(id)
       }else {
-         pos++
-         zombie.style.left = pos + "px"
-         zombie.style.bottom = pos + "px"
+         pos8++
+         zombie.style.left = pos8 + "px"
+         zombie.style.bottom = pos8 + "px"
       }
    }
    id = setInterval(frame, 5)
 }
 const spawnZombies = () => {
       let position = Math.floor(Math.random()*8)+1
-      const zombie = document.createElement("img")
-      zombie.src = "./assets/zombie.gif"
-      zombie.className = "zombie"
-      document.getElementById("zombie"+position).append(zombie)
-      console.log(position)
-      }
-
+      let zombiePosition =document.getElementById("zombie"+position)
+      if (zombiePosition.hasChildNodes() === false){
+         const zombie = document.createElement("img")
+         zombie.src = "./assets/zombie.gif"
+         zombie.className = "zombie"
+         zombiePosition.append(zombie)
+         zombieAtk()
+         }
+   }
+const zombieAtk = () => {
+   if (document.getElementById("zombie1").hasChildNodes() === true && pos1 === 0) {
+      moveZombie1()
+   }
+   if (document.getElementById("zombie2").hasChildNodes() === true && pos2 === 0) {
+      moveZombie2()
+   }
+   if (document.getElementById("zombie3").hasChildNodes() === true && pos3 === 0) {
+      moveZombie3()
+   }
+   if (document.getElementById("zombie4").hasChildNodes() === true && pos4 === 0) {
+      moveZombie4()
+   }
+   if (document.getElementById("zombie5").hasChildNodes() === true && pos5 === 0) {
+      moveZombie5()
+   }
+   if (document.getElementById("zombie6").hasChildNodes() === true && pos6 === 0) {
+      moveZombie6()
+   }
+   if (document.getElementById("zombie7").hasChildNodes() === true && pos7 === 0) {
+      moveZombie7()
+   }
+   if (document.getElementById("zombie8").hasChildNodes() === true && pos8 === 0) {
+      moveZombie8()
+   }
+}
