649c15d0c36cccea38275b21ed8e7840802da3ab
ImNGodmode <bjames.56@icloud.com>
2022-09-05 11:27:28 -0400

    added divs for the other zombies and logic to make them move

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 
 let sec = 30
 let time = document.getElementById("time")
-let timer;
 
 
-timer = setInterval(() => {
+const timer = setInterval(() => {
    time.innerText =  sec;
    sec--;
    console.log(sec);
@@ -24,6 +23,115 @@ const moveZombie1 = () => {
       }else {
          pos++
          zombie.style.left = pos + "px"
+      }
+   }
+   id = setInterval(frame, 5)
+}
+const moveZombie2 = () => {
+   let id = null
+   const zombie = document.getElementById('zombie2')
+   let pos = 0
+   clearInterval(id)
+   const frame = () => {
+      if (pos === 350) {
+         clearInterval(id)
+      }else {
+         pos++
+         zombie.style.right = pos + "px"
+      }
+   }
+   id = setInterval(frame, 5)
+}
+const moveZombie3 = () => {
+   let id = null
+   const zombie = document.getElementById('zombie3')
+   let pos = 0
+   clearInterval(id)
+   const frame = () => {
+      if (pos === 200) {
+         clearInterval(id)
+      }else {
+         pos++
+         zombie.style.top = pos + "px"
+      }
+   }
+   id = setInterval(frame, 5)
+}
+const moveZombie4 = () => {
+   let id = null
+   const zombie = document.getElementById('zombie4')
+   let pos = 0
+   clearInterval(id)
+   const frame = () => {
+      if (pos === 200) {
+         clearInterval(id)
+      }else {
+         pos++
+         zombie.style.bottom = pos + "px"
+      }
+   }
+   id = setInterval(frame, 5)
+}
+const moveZombie5 = () => {
+   let id = null
+   const zombie = document.getElementById('zombie5')
+   let pos = 0
+   clearInterval(id)
+   const frame = () => {
+      if (pos === 350) {
+         clearInterval(id)
+      }else {
+         pos++
+         zombie.style.right = pos + "px"
+         zombie.style.top = pos + "px"
+      }
+   }
+   id = setInterval(frame, 5)
+}
+const moveZombie6 = () => {
+   let id = null
+   const zombie = document.getElementById('zombie6')
+   let pos = 0
+   clearInterval(id)
+   const frame = () => {
+      if (pos === 350) {
+         clearInterval(id)
+      }else {
+         pos++
+         zombie.style.right = pos + "px"
+         zombie.style.bottom = pos + "px"
+      }
+   }
+   id = setInterval(frame, 5)
+}
+const moveZombie7 = () => {
+   let id = null
+   const zombie = document.getElementById('zombie7')
+   let pos = 0
+   clearInterval(id)
+   const frame = () => {
+      if (pos === 350) {
+         clearInterval(id)
+      }else {
+         pos++
+         zombie.style.left = pos + "px"
+         zombie.style.top = pos + "px"
+      }
+   }
+   id = setInterval(frame, 5)
+}
+const moveZombie8 = () => {
+   let id = null
+   const zombie = document.getElementById('zombie8')
+   let pos = 0
+   clearInterval(id)
+   const frame = () => {
+      if (pos === 350) {
+         clearInterval(id)
+      }else {
+         pos++
+         zombie.style.left = pos + "px"
+         zombie.style.bottom = pos + "px"
       }
    }
    id = setInterval(frame, 5)
